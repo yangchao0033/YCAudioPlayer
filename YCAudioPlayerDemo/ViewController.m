65842efd6d95ea9cbe49acb3d273cd221ec85b06
SuperYang <757097678@qq.com>
2015-11-10 14:49:28 +0800

    完成Demo测试

--- YCAudioPlayerDemo/ViewController.m
+++ YCAudioPlayerDemo/ViewController.m
@@ -28,8 +28,8 @@
 - (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event
 {
     NSLog(@"%s", __func__);
-    //从budle路径下读取音频文件　　轻音乐 - 萨克斯回家 这个文件名是你的歌曲名字,mp3是你的音频格式
-    NSString *string = [[NSBundle mainBundle] pathForResource:@"1" ofType:@"mp3"];
+    //从budle路径下读取音频文件　　Katy Perry - Roar 这个文件名是你的歌曲名字,mp3是你的音频格式
+    NSString *string = [[NSBundle mainBundle] pathForResource:@"Katy Perry - Roar" ofType:@"mp3"];
     //把音频文件转换成url格式
     NSURL *url = [NSURL fileURLWithPath:string];
     YCAudioPlayer *player = [YCAudioPlayer audioPlayerWithUrl:url];
