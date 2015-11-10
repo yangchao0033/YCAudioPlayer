cc544feb086af8cc0b60fa75cd45f7e53de772ca
SuperYang <757097678@qq.com>
2015-11-10 14:42:32 +0800

    初始化并添加需要的资源

---
+++ YCAudioPlayerDemo/ViewController.m
@@ -0,0 +1,31 @@
+//
+//  ViewController.m
+//  YCAudioPlayerDemo
+//
+//  Created by 超杨 on 15/11/10.
+//  Copyright © 2015年 杨超. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event
+{
+    NSLog(@"%s", __func__);
+}
+@end
